920ce3c13f40e3cb40682473d481a0151d3dd560
Arikapa <stevenaricapa@outlook.es>
2026-05-18 03:03:33 -0500

    Permite la foto del bus

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,50 +1,56 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { APP_GUARD } from '@nestjs/core';
 import { RutasModule } from './rutas/rutas.module';
 import { ParaderosModule } from './paraderos/paraderos.module';
 import { BoletosModule } from './boletos/boletos.module';
+import { NodosModule } from './nodos/nodos.module';
 import { HistoriasModule } from './historias/historias.module';
 import { EmpresasModule } from './empresas/empresas.module';
-import { BusesModule } from './buses/buses.module';
 import { GpsModule } from './gps/gps.module';
-import { ProgramacionesModule } from './programaciones/programaciones.module';
-import { MetodospagoModule } from './metodospago/metodospago.module';
-import { MetodospagociudadanoModule } from './metodospagociudadano/metodospagociudadano.module';
-import { ReportesModule } from './reportes/reportes.module';
-import { TransaccionesModule } from './transacciones/transacciones.module';
-import { NodosModule } from './nodos/nodos.module';
+import { BusesModule } from './buses/buses.module';
+import { ConductoresModule } from './conductores/conductores.module';
+import { IncidentesModule } from './incidentes/incidentes.module';
+import { TurnosModule } from './turnos/turnos.module';
+import { NotificacionesModule } from './gateways/notifications/notifications.module';
+import { SecurityGuard } from './guards/security.guard';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'mysql',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USER'),
-        password: configService.get<string>('DB_PASS'),
-        database: configService.get<string>('DB_NAME'),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: false, // Usaremos migraciones
-      }),
-    }),
-    RutasModule,
-    ParaderosModule,
-    BoletosModule,
-    NodosModule,
-    HistoriasModule,
-    EmpresasModule,
-    BusesModule,
-    GpsModule,
-    ProgramacionesModule,
-    MetodospagoModule,
-    MetodospagociudadanoModule,
-    ReportesModule,
-    TransaccionesModule,
-  ],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                type: 'mysql',
+                host: configService.get<string>('DB_HOST'),
+                port: configService.get<number>('DB_PORT'),
+                username: configService.get<string>('DB_USER'),
+                password: configService.get<string>('DB_PASS'),
+                database: configService.get<string>('DB_NAME'),
+                entities: [__dirname + '/**/*.entity{.ts,.js}'],
+                synchronize: false,
+            }),
+        }),
+        RutasModule,
+        ParaderosModule,
+        BoletosModule,
+        NodosModule,
+        HistoriasModule,
+        EmpresasModule,
+        GpsModule,
+        BusesModule,
+        ConductoresModule,
+        TurnosModule,
+        IncidentesModule,
+        NotificacionesModule,
+    ],
+    providers: [
+        {
+            provide: APP_GUARD,
+            useClass: SecurityGuard,  // ← NestJS inyecta Reflector automáticamente
+        },
+    ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,27 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { SecurityGuard } from './guards/security.guard';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  
-  app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
-    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  });
+    const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, transform: true }));
-  app.useGlobalGuards(new SecurityGuard());
-  await app.listen(process.env.PORT ?? 3000);
+    app.use(bodyParser.json({ limit: '10mb' }));
+    app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+
+    app.enableCors({
+        origin: ['http://localhost:5173', 'http://localhost:5174'],
+        methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        credentials: true,
+    });
+
+    app.useGlobalPipes(new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+    }));
+
+    await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
