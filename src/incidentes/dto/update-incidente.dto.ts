632c306fdd4ed9540f120a894999ac2b8bc2c1e4
Arikapa <stevenaricapa@outlook.es>
2026-05-19 10:02:52 -0500

    Actualización y creación incidentes

--- src/incidentes/dto/update-incidente.dto.ts
+++ src/incidentes/dto/update-incidente.dto.ts
@@ -1,4 +1,19 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateIncidenteDto } from './create-incidente.dto';
+import { IsIn, IsOptional, IsString, MaxLength, MinLength } from "class-validator";
 
-export class UpdateIncidenteDto extends PartialType(CreateIncidenteDto) {}
+export class UpdateIncidenteDto {
+    @IsString()
+    @IsOptional()
+    @IsIn(['pendiente', 'en_revision', 'resuelto'])
+    estado?: string;
+
+    @IsString()
+    @IsOptional()
+    @MinLength(10)
+    @MaxLength(500)
+    descripcion?: string;
+
+    @IsString()
+    @IsOptional()
+    @IsIn(['bajo', 'medio', 'alto'])
+    gravedad?: string;
+}
